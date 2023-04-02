df9533c0a4613905b46758f3dd4722799cb5df53
koursari <126717841+koursari@users.noreply.github.com>
2023-04-02 17:24:26 +0300

    Show locations based on a location graph

--- scripts.js
+++ scripts.js
@@ -1,3 +1,73 @@
+//Populate Locations List
+let locations = [
+    "Anor Londo",
+    "Undead Parish",
+    "Depths",
+    "Sen's Fortress",
+    "Darkroot Basin",
+    "Blighttown",
+    "Ashen Lake"
+];
+let connections = [
+    [3],
+    [2,3,4],
+    [1,5],
+    [0,1],
+    [1],
+    [2,6],
+    [5]
+
+];
+let originLocation = document.getElementById("from-location");
+originLocation.value = "";
+locations.forEach(i => {
+    let loc = document.createElement("option");
+    originLocation.appendChild(loc);
+    loc.textContent = i;
+});
+
+let destinationLocation = document.getElementById("destination-location");
+destinationLocation.value = "";
+
+//Pick Destination based on Origin and Available Connections
+function updateDestinations() {
+    //clear Departures List and disable button
+    let departureForm = document.getElementById("departures");
+    departureForm.style.display = "none";
+    departureForm.querySelector("ul").innerHTML = "";
+    let btn = document.getElementById("searchtrips");
+    btn.disabled = true;
+
+    //find what the user chose
+    let originIndex = locations.indexOf(originLocation.value);
+    //update available destinations
+    let destinationLocation = document.getElementById("destination-location");
+    destinationLocation.innerHTML = ""; //clear destinations
+    //write a placeholder option
+    let emptyDestination = document.createElement("option");
+    destinationLocation.appendChild(emptyDestination);
+    emptyDestination.setAttribute("value", "");
+    emptyDestination.setAttribute("disabled", "true");
+    emptyDestination.setAttribute("selected", "true");
+    emptyDestination.setAttribute("hidden", "true");
+    emptyDestination.textContent = "Προορισμός";
+    //write actual available destinations
+    connections[originIndex].forEach(i => {
+        let loc = document.createElement("option");
+        destinationLocation.appendChild(loc);
+        loc.textContent = locations[i];
+    });
+}
+originLocation.addEventListener("change", updateDestinations);
+
+document.getElementById("destination-location").addEventListener("change", () => {
+    let departureForm = document.getElementById("departures");
+    departureForm.style.display = "none";
+    departureForm.querySelector("ul").innerHTML = "";
+    let btn = document.getElementById("searchtrips");
+    btn.disabled = false;
+});
+
 //Populate Departures List
 let customDepartures = [
     {
@@ -11,7 +81,14 @@ let customDepartures = [
 ];
 
 function populateDepartures(){
+    let locationA = document.getElementById("from-location").value;
+    let locationB = document.getElementById("destination-location").value;
+
     let departureForm = document.getElementById("departures");
+    let tripSpan = document.createElement("span");
+    tripSpan.textContent = "Από: " + locationA + ". Προς: " + locationB + ".";
+    departureForm.appendChild(tripSpan);
+
     let formUL = departureForm.querySelector("ul");
     let btn = departureForm.querySelector("button");
     btn.disabled = true;
@@ -33,7 +110,7 @@ function populateDepartures(){
         deRadio.setAttribute("id", i.timeID);
         deRadio.addEventListener("change", ()=>{btn.disabled = false});
         deLabel.textContent = i.text;
-        deLabel.setAttribute("for", li.timeID);
+        deLabel.setAttribute("for", i.timeID);
     });
     departureForm.style.display = "block";
 }
