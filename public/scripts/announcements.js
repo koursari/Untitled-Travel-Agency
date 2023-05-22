4ace6ee62df0e4124b5e45fd389cef28c00348ae
koursari <126717841+koursari@users.noreply.github.com>
2023-05-22 05:06:54 +0300

    transform announcement list to handlebar format
    This commit changes the way the announcement list in the sidebar is
    populated. Instead of it being hard-coded in the clientside js file,
    it is rendered serverside.
    The only problem is that I have not yet created the appropriate database
    support for it, so the information remain hardcoded (in a js file in the
    root directory instead)

--- public/scripts/announcements.js
+++ public/scripts/announcements.js
@@ -1,29 +1,3 @@
-//Populate Announcement List
-let customAnnouncements = [
-{
-    title: "Prima",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-},
-{
-    title: "Secunda",
-    text: "I am the very model of a modern Major-Gineral, I've information vegetable, animal, and mineral, I know the kings of England, and I quote the fights historical, from Marathon to Waterloo, in order categorical.",
-},
-{
-    title: "Tetria",
-    text: "The FitnessGram Pacer test is a multistage aerobic capacity test that progressively gets more difficult as it continues."
-}
-];
-let sidebarDiv = document.getElementById("sidebar");
-
-let announcementUL = document.createElement("ul");
-sidebarDiv.appendChild(announcementUL);
-customAnnouncements.forEach(i => {
-    let an = document.createElement("li");
-    an.textContent = i.title;
-    an.setAttribute("data-text", i.text);
-    announcementUL.appendChild(an);
-});
-
 //Click on sidebar announcements should lead to a custom pop up
 function announce(event){
     let x = document.getElementById("short-announce");
