f75968a1e28c4e67bbe6d4c32a9b05f5a9645dd0
koursari <126717841+koursari@users.noreply.github.com>
2023-05-25 11:58:17 +0300

    separate page assembly from routing

---
+++ routes/page-components.mjs
@@ -0,0 +1,33 @@
+const announcementsController = await import(`../controller/announcements-controller.mjs`);
+const travelController = await import(`../controller/travel-controller.mjs`);
+
+export async function homepage(request, response) {
+    let announcements = await announcementsController.getAnnouncements();
+    response.render('index',
+    {
+        layout: 'userContent.hbs',
+        locationList: null,
+        connections: null,
+        announcementList: announcements
+    })
+}
+
+export async function aboutpage(request, response) {
+    let announcements = await announcementsController.getAnnouncements();
+    response.render('about',
+    {
+        layout: 'userContent.hbs',
+        locationList: null,
+        connections: null,
+        announcementList: announcements
+    })
+}
+
+export async function adminDashboard(request, response) {
+    let announcements = await announcementsController.getAnnouncements();
+    response.render('admin',
+    {
+        layout: 'userContent.hbs',
+        announcementList: announcements
+    })
+}
--- routes/routes.mjs
+++ routes/routes.mjs
@@ -1,73 +1,20 @@
 import express from 'express'
-const announcementsController = await import(`../controller/announcements-controller.mjs`);
+const pages = await import(`./page-components.mjs`)
 
 const router = express.Router();
 
-//router.route('/').get((request, response) => {response.render('index', {layout: 'userContent.hbs'})});
-router.route('/index').get(async (request, response) => {
-    let announcements = await announcementsController.getAnnouncements();
-    response.render('index',
-    {
-        layout: 'userContent.hbs',
-        locationList: null,
-        connections: null,
-        announcementList: announcements
-    })
-});
-router.route('/about').get(async (request, response) => {
-    let announcements = await announcementsController.getAnnouncements();
-    response.render('about',
-    {
-        layout: 'userContent.hbs',
-        announcementList: announcements
-    })
-});
-router.route('/profile').get(async (request, response) => {
-    let announcements = await announcementsController.getAnnouncements();
-    response.render('profile',
-    {
-        layout: 'userContent.hbs',
-        announcementList: announcements
-    })
-});
+router.route('/').get(pages.homepage);
+router.route('/home').get(pages.homepage);
 
-const travelController = await import(`../controller/travel-controller.mjs`)
+router.route('/about').get(pages.aboutpage);
+router.route('/profile').get(pages.adminDashboard);
 
+router.route('/admin/flights').get(pages.adminDashboard);
+router.route('/admin/users').get(pages.adminDashboard);
+router.route('/admin/tickets').get(pages.adminDashboard);
 
-router.route('/admin/flights').get((request, response) => {
-    response.render('about',
-    {
-        layout: 'userContent.hbs',
-        announcementList: customAnnouncements
-    })
-});
-router.route('/admin/users').get((request, response) => {
-    response.render('about',
-    {
-        layout: 'userContent.hbs',
-        announcementList: customAnnouncements
-    })
-});
-router.route('/admin/tickers').get((request, response) => {
-    response.render('about',
-    {
-        layout: 'userContent.hbs',
-        announcementList: customAnnouncements
-    })
-});
-
-//Καταχώριση συμπεριφοράς σε διάφορα path
-// router.route('/').get((req, res) => { res.redirect('/flights') });
-router.route('/').get((request, response) => {
-    response.render('home')
-});
-
-router.get('/flights/remove/:removeFlightId', travelController.removeFlight);
-router.get('/flights/add/', travelController.addFlight);
-router.get('/flights', travelController.listAllFlights);
-
-router.get('/users', travelController.listAllUsers);
-
-router.get('/tickets', travelController.listAllTickets);
-
+/*
+//router.get('/flights/remove/:removeFlightId', travelController.removeFlight);
+//router.get('/flights/add/', travelController.addFlight);
+*/
 export { router };
