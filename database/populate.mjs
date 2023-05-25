24517888c1af4d093d98f1a6d273f659b086f54a
koursari <126717841+koursari@users.noreply.github.com>
2023-05-25 15:33:05 +0300

    await every insert and exit in populate module

--- database/populate.mjs
+++ database/populate.mjs
@@ -20,7 +20,7 @@ let admin = [{
 //   });
 
 admin[0].password = bcrypt.hashSync(admin[0].password, saltRounds);
-pool.query('INSERT INTO admin(username, password) VALUES ($1, $2)', [admin[0].username, admin[0].password]);
+await pool.query('INSERT INTO admin (username, password) VALUES ($1, $2)', [admin[0].username, admin[0].password]);
 
 //Populate users table with 3 entries
 let user = [{
@@ -50,7 +50,7 @@ let user = [{
 
 for (let i = 0; i < user.length; i++) {
     let hashedPassword = bcrypt.hashSync(user[i].password, 10);
-    pool.query('INSERT INTO users (username, password, first_name, last_name, phone, address) VALUES ($1, $2, $3, $4, $5, $6)', [user[i].username, hashedPassword, user[i].first_name, user[i].last_name, user[i].phone, user[i].address]);   
+    await pool.query('INSERT INTO users (username, password, first_name, last_name, phone, address) VALUES ($1, $2, $3, $4, $5, $6)', [user[i].username, hashedPassword, user[i].first_name, user[i].last_name, user[i].phone, user[i].address]);   
 }
 
 
@@ -102,7 +102,7 @@ let flights = [{
 }];
 
 for (let i = 0; i<flights.length; i++) {
-    pool.query('INSERT INTO flight(f_id, company, departure, d_date, destination, a_date, t_f_seats, first, t_b_seats, business, t_e_seats, economy, admin_username) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)',
+    await pool.query('INSERT INTO flight(f_id, company, departure, d_date, destination, a_date, t_f_seats, first, t_b_seats, business, t_e_seats, economy, admin_username) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)',
     [flights[i].f_id, flights[i].company, flights[i].departure, flights[i].d_date, flights[i].destination, flights[i].a_date, flights[i].t_f_seats, flights[i].first, flights[i].t_b_seats, flights[i].business, flights[i].t_e_seats, flights[i].economy, flights[i].admin_username]);
 }
 
@@ -133,7 +133,8 @@ let announcements = [{
 }];
 
 for (let i=0; i<announcements.length; i++) {
-    pool.query('INSERT INTO announcements (id, title, content, status, date, admin_username) VALUES ($1, $2, $3, $4, $5, $6)', [
+    await pool.query('INSERT INTO announcements (id, title, content, status, date, admin_username) VALUES ($1, $2, $3, $4, $5, $6)', [
         announcements[i].id, announcements[i].title, announcements[i].content, announcements[i].status, announcements[i].date, announcements[i].admin_username]);
 }
 
+process.exit(0);
