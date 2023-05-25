0a6dfc452e0ccd63f6aa0932a93423b126e8eea7
koursari <126717841+koursari@users.noreply.github.com>
2023-05-25 19:27:03 +0300

    recreate page builder system after messy merge

--- routes/routes.mjs
+++ routes/routes.mjs
@@ -1,6 +1,4 @@
 import express from 'express'
-import { customAnnouncements } from '../database/announcements.mjs'
-import { locations, directConnections } from '../database/locations.mjs'
 import passport from 'passport';
 
 const pages = await import(`./page-components.mjs`)
@@ -9,56 +7,29 @@ const router = express.Router();
 
 // const userModel = await import(`../controller/user-controller.mjs`)
 
+router.route('/').get(pages.homepage);
+router.route('/home').get(pages.homepage);
+router.route('/about').get(pages.aboutpage);
 
 
+router.route('/profile').get(pages.adminDashboard);
 
-//router.route('/').get((request, response) => {response.render('index', {layout: 'userContent.hbs'})});
-router.route('/index').get((request, response) => {
-    response.render('index',
-        {
-            layout: 'userContent.hbs',
-            locationList: locations,
-            connections: directConnections,
-            announcementList: customAnnouncements
-        })
-});
-router.route('/about').get((request, response) => {
-    response.render('about',
-        {
-            layout: 'userContent.hbs',
-            announcementList: customAnnouncements
-        })
-});
-//THIS IS THE ADMIN DASHBOARD
-//TO BE CHANGED WHEN WE START DOING SESSIONS
-//PROFILE SHOULD REDIRECT TO LOGIN
-router.route('/profile').get((request, response) => {
-    response.render('admin',
-        {
-            layout: 'userContent.hbs',
-            announcementList: customAnnouncements
-        })
-});
+router.route('/admin/flights').get(pages.flightsView);
+router.route('/admin/users').get(pages.usersView);
+router.route('/admin/tickets').get(pages.ticketsView);
 
+router.get('/admin/flights/add/', pages.manageFlightAdd);
+router.get('/admin/flights/remove/:removeFlightId', pages.manageFlightRemove);
 
 //USER MANAGEMENT
+//router.route('/login').get(pages.loginpage);
 router.get('/login', checkAuthenticated, (req, res) => {
     res.render('login',
         {
             layout: 'userContent.hbs',
-            announcementList: customAnnouncements
+            announcementList: null
         })
 })
-
-//previous
-// router.route('/login').get((request, response) => { 
-//     response.render('login',
-//     {
-//         layout: 'userContent.hbs',
-//         announcementList: customAnnouncements
-//     })
-// });
-
 
 router.post('/login/user',
     passport.authenticate('local', {
@@ -67,50 +38,6 @@ router.post('/login/user',
         failureFlash: true
     })
 );
-
-const travelController = await import(`../controller/travel-controller.mjs`)
-
-router.route('/login').get(pages.loginpage);
-
-router.route('/admin/flights').get((request, response) => {
-    response.render('about',
-        {
-            layout: 'userContent.hbs',
-            announcementList: customAnnouncements
-        })
-});
-router.route('/admin/users').get((request, response) => {
-    response.render('about',
-        {
-            layout: 'userContent.hbs',
-            announcementList: customAnnouncements
-        })
-});
-router.route('/admin/tickers').get((request, response) => {
-    response.render('about',
-        {
-            layout: 'userContent.hbs',
-            announcementList: customAnnouncements
-        })
-});
-
-
-//Καταχώριση συμπεριφοράς σε διάφορα path
-// router.route('/').get((req, res) => { res.redirect('/flights') });
-router.route('/').get((request, response) => {
-    response.render('home')
-});
-
-router.get('/flights/remove/:removeFlightId', travelController.removeFlight);
-router.get('/flights/add/', travelController.addFlight);
-router.get('/flights', travelController.listAllFlights);
-
-router.get('/users', travelController.listAllUsers);
-
-router.get('/tickets', travelController.listAllTickets);
-
-
-
 
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
