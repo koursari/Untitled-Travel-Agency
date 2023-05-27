643f05c73db6aef688a27525c2f439a5b13b737f
koursari <126717841+koursari@users.noreply.github.com>
2023-05-27 19:24:53 +0300

    filter flights based on user selection

--- public/scripts/trip-planner.js
+++ public/scripts/trip-planner.js
@@ -1,75 +1,98 @@
 try {
-const connectionInformation = document.getElementById("direct-connections").querySelectorAll("div");
-const connections = new Array();
-connectionInformation.forEach(i => {
-    let from = i.dataset.from;
-    let to = i.dataset.to;
-    connections.push([from, to]);
-});
 
-let originSelector = document.getElementById("from-location");
-let destinationSelector = document.getElementById("destination-location");
-originSelector.value = "";
-destinationSelector.value = "";
-
-function getNeighbors(x) {
-    let result = new Array();
-    connections.forEach(element => {
-        if (element[0] == x) result.push(element[1])
-    })
-    return result;
-}
-
-function resetFlightsList() {
-    let departureForm = document.getElementById("departures");
-    departureForm.style.display = "none";
-    //departureForm.querySelector("ul").innerHTML = "";
-    let btn = document.getElementById("searchtrips");
-    btn.disabled = true;
-}
-
-function updateDestinations() {
-    //Hde all connections
-    destinations = destinationSelector.querySelectorAll("option");
-    destinations.forEach(element => {
-        element.hidden = true;
-        element.disabled = true;
+    const connectionInformation = document.getElementById("direct-connections").querySelectorAll("div");
+    const connections = new Array();
+    connectionInformation.forEach(i => {
+        let from = i.dataset.from;
+        let to = i.dataset.to;
+        connections.push([from, to]);
     });
+
+    let originSelector = document.getElementById("from-location");
+    let destinationSelector = document.getElementById("destination-location");
+    originSelector.value = "";
     destinationSelector.value = "";
-    //then show direct connections
-    let selectedOriginID = originSelector.value;
-    neighbors = getNeighbors(selectedOriginID);
-    destinations.forEach(element => {
-        id = element.value;
-        if(neighbors.includes(id)) {
-            element.hidden = false;
-            element.disabled = false;
-        }
-    });
-}
-function changeInOrigin() {
-    resetFlightsList();
-    updateDestinations();
-}
-function changeInDestination() {
-    resetFlightsList();
-    let btn = document.getElementById("searchtrips");
-    btn.disabled = false;
-}
 
-originSelector.addEventListener("change", changeInOrigin);
-destinationSelector.addEventListener("change", changeInDestination);
-
-function filterDepartures() {
-    //hide unrelated flights
-    
-    //show departures table
     let departureForm = document.getElementById("departures");
-    departureForm.style.display = "block";
-}
+    let ticketSubmit = departureForm.querySelector("button");
+    ticketSubmit.disabled = true;
+    
+    let departureFormFlights = departureForm.querySelectorAll("input");
+    departureFormFlights.forEach(element => {
+        element.removeAttribute("checked");
+        element.addEventListener("change", ()=>{ticketSubmit.disabled = false})
+    });
 
-let searchButton = document.getElementById("searchtrips");
-searchButton.addEventListener("click", filterDepartures);
+
+    function getNeighbors(x) {
+        let result = new Array();
+        connections.forEach(element => {
+            if (element[0] == x) result.push(element[1])
+        })
+        return result;
+    }
+
+    function resetFlightsList() {
+        
+        departureForm.style.display = "none";
+        ticketSubmit.disabled = true;
+        let btn = document.getElementById("searchtrips");
+        btn.disabled = true;
+    }
+
+    function updateDestinations() {
+        //Hde all connections
+        destinations = destinationSelector.querySelectorAll("option");
+        destinations.forEach(element => {
+            element.hidden = true;
+            element.disabled = true;
+        });
+        destinationSelector.value = "";
+        //then show direct connections
+        let selectedOriginID = originSelector.value;
+        neighbors = getNeighbors(selectedOriginID);
+        destinations.forEach(element => {
+            id = element.value;
+            if (neighbors.includes(id)) {
+                element.hidden = false;
+                element.disabled = false;
+            }
+        });
+    }
+    function changeInOrigin() {
+        resetFlightsList();
+        updateDestinations();
+    }
+    function changeInDestination() {
+        resetFlightsList();
+        let btn = document.getElementById("searchtrips");
+        btn.disabled = false;
+    }
+
+    originSelector.addEventListener("change", changeInOrigin);
+    destinationSelector.addEventListener("change", changeInDestination);
+
+    function filterDepartures() {
+        let formLegend = departureForm.querySelector("legend");
+        let locationA = originSelector.value;
+        let locationB = destinationSelector.value;
+        formLegend.innerText = "Δρομολόγια: " + locationA + " - " + locationB;
+
+        //hide unrelated flights
+        let flightListDivs = departureForm.querySelectorAll("div");
+        flightListDivs.forEach(element => {
+            if ((element.dataset.from == locationA) && (element.dataset.to == locationB)) {
+                element.style.display = "block";
+            } else {
+                element.style.display = "none";
+            }
+            console.log();
+        });
+        departureForm.style.display = "block";
+    }
+
+    let searchButton = document.getElementById("searchtrips");
+    searchButton.addEventListener("click", filterDepartures);
 
 } catch {
     console.log("Oops, wrong page")
