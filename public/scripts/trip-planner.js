356462c141a493b7a94acccacdc19860bfd5927c
koursari <126717841+koursari@users.noreply.github.com>
2023-05-27 18:56:18 +0300

    show flight list in index

--- public/scripts/trip-planner.js
+++ public/scripts/trip-planner.js
@@ -22,7 +22,7 @@ function getNeighbors(x) {
 function resetFlightsList() {
     let departureForm = document.getElementById("departures");
     departureForm.style.display = "none";
-    departureForm.querySelector("ul").innerHTML = "";
+    //departureForm.querySelector("ul").innerHTML = "";
     let btn = document.getElementById("searchtrips");
     btn.disabled = true;
 }
@@ -50,71 +50,26 @@ function changeInOrigin() {
     resetFlightsList();
     updateDestinations();
 }
-let changeInDestination = resetFlightsList;
+function changeInDestination() {
+    resetFlightsList();
+    let btn = document.getElementById("searchtrips");
+    btn.disabled = false;
+}
 
 originSelector.addEventListener("change", changeInOrigin);
 destinationSelector.addEventListener("change", changeInDestination);
-} catch {
-    console.log("Oops, wrong page")
-}
-/*
-//Populate Departures List
-let customDepartures = [
-    {
-        timeID: "time1",
-        text: "Δρομολόγιο 1",
-    },
-    {
-        timeID: "time2",
-        text: "Δρομολόγιο 2",
-    }
-];
 
-function populateDepartures(){
-    let locationA = document.getElementById("from-location").value;
-    let locationB = document.getElementById("destination-location").value;
-
+function filterDepartures() {
+    //hide unrelated flights
+    
+    //show departures table
     let departureForm = document.getElementById("departures");
-    let formUL = departureForm.querySelector("ul");
-    let btn = departureForm.querySelector("button");
-    btn.disabled = true;
-    if (formUL.innerHTML != "") {
-        formUL.innerHTML = "";
-        departureForm.style.display = "none";
-        return;
-    }
-    customDepartures.forEach(i => {
-        let deLI = document.createElement("li");
-        let deRadio = document.createElement("input");
-        let deLabel = document.createElement("label");
-        deLI.appendChild(deRadio);
-        deLI.appendChild(deLabel);
-        formUL.appendChild(deLI);
-        deRadio.setAttribute("type", "radio");
-        deRadio.setAttribute("name", "time");
-        deRadio.setAttribute("value", i.timeID);
-        deRadio.setAttribute("id", i.timeID);
-        deRadio.addEventListener("change", ()=>{btn.disabled = false});
-        deLabel.textContent = i.text;
-        deLabel.setAttribute("for", i.timeID);
-    });
-
-    //to make the form more accessible
-    let La = document.createElement("input");
-    La.setAttribute("name","origin");
-    La.setAttribute("value", locationA);
-    La.style.display = "none";
-    formUL.appendChild(La);
-
-    let Lb = document.createElement("input");
-    Lb.setAttribute("name","destination");
-    Lb.setAttribute("value", locationB);
-    Lb.style.display = "none";
-    formUL.appendChild(Lb);
-
     departureForm.style.display = "block";
 }
 
 let searchButton = document.getElementById("searchtrips");
-searchButton.addEventListener("click", populateDepartures);
-*/
+searchButton.addEventListener("click", filterDepartures);
+
+} catch {
+    console.log("Oops, wrong page")
+}
