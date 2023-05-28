c392b76fc807420c4c4e3c7c3153ebe494cd79bd
koursari <126717841+koursari@users.noreply.github.com>
2023-05-28 20:07:57 +0300

    complete-er announcements look

--- public/scripts/announcements.js
+++ public/scripts/announcements.js
@@ -2,7 +2,14 @@
 function announce(event){
     let x = document.getElementById("short-announce");
     x.style.display = "block";
-    x.querySelector("span").textContent = event.currentTarget.getAttribute("data-text");
+    let popTitle = x.querySelector("#pop-title");
+    let popDate = x.querySelector("#pop-date");
+    let popContent = x.querySelector("#pop-content");
+    popTitle.innerText = event.currentTarget.dataset.title;
+    let dateFormat = new Date(event.currentTarget.dataset.date);
+    popDate.innerText = dateFormat.toLocaleDateString();
+    popContent.innerText = event.currentTarget.dataset.text;
+
     let btn = x.querySelector("button").addEventListener("click", ()=>{x.style.display = "none"})
 }
 
