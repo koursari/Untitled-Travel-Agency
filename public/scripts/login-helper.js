c2b12e1d7f3a7cd91b3b031b46f88c6807ecd9c2
koursari <126717841+koursari@users.noreply.github.com>
2023-05-28 21:09:26 +0300

    cleanup of the login page

--- public/scripts/login-helper.js
+++ public/scripts/login-helper.js
@@ -3,5 +3,16 @@ if (!(notRegisteredBtn == null)) {
     //hide register form
     let registerForm = document.getElementById("user-signup-area");
     registerForm.style.display = 'none';
-    notRegisteredBtn.addEventListener("click", () => {registerForm.style.display = 'block'})
+
+    notRegisteredBtn.addEventListener("click", statusSwap)
+
+    function statusSwap() {
+        if(registerForm.style.display == 'none') {
+            notRegisteredBtn.innerText = "Έχω τελικά"
+            registerForm.style.display = 'block'   
+        } else {
+            notRegisteredBtn.innerText = "Δεν έχω προφίλ"
+            registerForm.style.display = 'none' 
+        }
+    }
 }
